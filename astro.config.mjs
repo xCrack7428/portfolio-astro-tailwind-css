e55792ed2b6373f2f48210ecdb13e10b9c2b4e5e
Mario Moñino Larrosa <cucoelcrack76@gmail.com>
2026-03-12 18:39:52 +0100

    Subida Configuracion de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://xCrack7428.github.io',
+  base: 'portfolio-astro-tailwind-css',
   integrations: [tailwind()]
 });
