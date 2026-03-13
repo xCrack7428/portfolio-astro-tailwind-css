7d2fc0fcf00f236b1699a15d5877474b6f30f4f0
Mario Moñino Larrosa <cucoelcrack76@gmail.com>
2026-03-13 11:26:59 +0100

    Mejoras Añadidas al Portffolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://xCrack7428.github.io',
-  base: 'portfolio-astro-tailwind-css',
+  base: '/mi-portfolio',
   integrations: [tailwind()]
 });
